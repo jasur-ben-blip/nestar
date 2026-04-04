08eb2459a52fd9cf4db71415fed2d3808a897796
Jasur(BEN) <abdujambulovj@gmail.com>
2026-04-04 18:27:59 +0900

    feat: develop signup business logic

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Member } from '../../libs/dto/member/member';
+import { MemberInput } from '../../libs/dto/member/member.input';
 
 @Injectable()
 export class MemberService {
@@ -8,8 +10,17 @@ export class MemberService {
     @InjectModel('Member') private readonly memberModel: Model<null>,
   ) {}
 
-  public async signup(): Promise<string> {
-    return 'Signup executed';
+  public async signup(input: MemberInput): Promise<Member> {
+    // TODO: Hash Password
+
+    try {
+      const result = await this.memberModel.create(input);
+      //TODO : Authentication via TOKEN
+      return result;
+    } catch (err) {
+      console.log('Error, Service.model:', err);
+      throw new BadRequestException(err);
+    }
   }
 
   public async login(): Promise<string> {
