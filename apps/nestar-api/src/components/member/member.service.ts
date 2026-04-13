96cc37101559151a50517becc7d6f80fae74f523
Jasur(BEN) <abdujambulovj@gmail.com>
2026-04-14 01:25:46 +0900

    feat: develop getAgents graphQl api

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -5,10 +5,14 @@ import {
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, ObjectId } from 'mongoose';
-import { Member } from '../../libs/dto/member/member';
-import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
-import { MemberStatus } from '../../libs/enums/member.enum';
-import { Messages } from '../../libs/enums/command.enum';
+import { Member, Members } from '../../libs/dto/member/member';
+import {
+  AgentsInquiry,
+  LoginInput,
+  MemberInput,
+} from '../../libs/dto/member/member.input';
+import { MemberStatus, MemberType } from '../../libs/enums/member.enum';
+import { Direction, Messages } from '../../libs/enums/command.enum';
 import { AuthService } from '../auth/auth.service';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
 import { T } from '../../libs/types/command';
@@ -119,6 +123,41 @@ export class MemberService {
     }
 
     return targetMember;
+  }
+
+  public async getAgents(
+    memberId: ObjectId,
+    input: AgentsInquiry,
+  ): Promise<Members> {
+    const { text } = input.search;
+    const match: T = {
+      memberType: MemberType.AGENT,
+      memberStatus: MemberStatus.ACTIVE,
+    };
+    const sort: T = {
+      [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC,
+    };
+    if (text) match.memberNick = { $regex: new RegExp(text, 'i') };
+    console.log('match:', match);
+    const result = await this.memberModel
+      .aggregate([
+        { $match: match },
+        { $sort: sort },
+        {
+          $facet: {
+            list: [
+              { $skip: (input.page - 1) * input.limit },
+              { $limit: input.limit },
+            ],
+            metaCounter: [{ $count: 'total' }],
+          },
+        },
+      ])
+      .exec();
+    console.log('result:', result);
+    if (!result.length)
+      throw new InternalServerErrorException(Messages.NO_DATA_FOUND);
+    return result[0];
   }
 
   public async getAllMembersByAdmin(): Promise<string> {
