910a3390ffb29481854a103f146bda4710a147f3
Jasur(BEN) <abdujambulovj@gmail.com>
2026-04-18 15:20:20 +0900

    feat:develop getProperties GraphQL api

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -14,6 +14,16 @@ export const availableMemberSorts = [
   'memberViews',
 ];
 
+export const availableOptions = ['propertyBarter', 'propertyRent'];
+export const availablePropertySort = [
+  'createdAt',
+  'updatedAt',
+  'propertyLikes',
+  'propertyViews',
+  'propertyRank',
+  'propertyPrice',
+];
+
 // IMAGE CONFIGURATION
 import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path';
@@ -26,4 +36,13 @@ export const getSerialForImage = (filename: string) => {
 
 export const shapeIntoMongoObjectId = (target: any) => {
   return typeof target === 'string' ? new ObjectId(target) : target;
+};
+
+export const lookupMember = {
+  $lookup: {
+    from: 'members',
+    localField: 'memberId',
+    foreignField: '_id',
+    as: 'memberData',
+  },
 };
