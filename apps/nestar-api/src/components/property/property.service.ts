ab04bf3bca3a75202edf46b9b886a7a3974d4cf9
Jasur(BEN) <abdujambulovj@gmail.com>
2026-04-18 14:26:22 +0900

    feat: develop updateProperty GraphQL api

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -13,6 +13,8 @@ import { StatisticModifier, T } from '../../libs/types/command';
 import { PropertyStatus } from '../../libs/enums/property.enum';
 import { ViewGroup } from '../../libs/enums/view.enum';
 import { ViewService } from '../view/view.service';
+import moment from 'moment';
+import { PropertyUpdate } from '../../libs/dto/property/property.update';
 
 @Injectable()
 export class PropertyService {
@@ -96,5 +98,38 @@ export class PropertyService {
         { new: true },
       )
       .exec();
+  }
+
+  public async updateProperty(
+    memberId: ObjectId,
+    input: PropertyUpdate,
+  ): Promise<Property> {
+    let { propertyStatus, soldAt, deletedAt } = input;
+    const search: T = {
+      _id: input._id,
+      memberId: memberId,
+      propertyStatus: PropertyStatus.ACTIVE,
+    };
+
+    if (propertyStatus === PropertyStatus.SOLD) soldAt = moment().toDate();
+    else if (propertyStatus === PropertyStatus.DELETE)
+      deletedAt = moment().toDate();
+
+    const result = await this.propertyModel
+      .findOneAndUpdate(search, input, {
+        new: true,
+      })
+      .exec();
+    if (!result) throw new InternalServerErrorException(Messages.UPDATE_FAILED);
+
+    if (soldAt || deletedAt) {
+      await this.memberService.memberStatsEditor({
+        _id: memberId,
+        targetKey: 'memberProperties',
+        modifier: -1,
+      });
+    }
+
+    return result;
   }
 }
