1cfd27c9c4667fb260fdf5f5426f7c0d3d2cc6da
Jasur(BEN) <abdujambulovj@gmail.com>
2026-04-18 15:50:26 +0900

    feat: develop getAllPropertiesByAdmin GraphQl api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -3,6 +3,7 @@ import { PropertyService } from './property.service';
 import { Properties, Property } from '../../libs/dto/property/property';
 import {
   AgentPropertiesInquiry,
+  AllPropertiesInquiry,
   PropertiesInquiry,
   PropertyInput,
 } from '../../libs/dto/property/property.input';
@@ -75,5 +76,18 @@ export class PropertyResolver {
   ): Promise<Properties> {
     console.log('Query: getAgentProperties');
     return await this.propertyService.getAgentProperties(memberId, input);
+  }
+
+  /**  ADMIN  **/
+
+  @Roles(MemberType.ADMIN)
+  @UseGuards(RolesGuard)
+  @Query((returns) => Properties)
+  public async getAllPropertiesByAdmin(
+    @Args('input') input: AllPropertiesInquiry,
+    @AuthMember('_id') memberId: ObjectId,
+  ): Promise<Properties> {
+    console.log('Query: getAllPropertiesByAdmin');
+    return await this.propertyService.getAllPropertiesByAdmin(input);
   }
 }
--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -9,6 +9,7 @@ import { Properties, Property } from '../../libs/dto/property/property';
 import { Direction, Messages } from '../../libs/enums/command.enum';
 import {
   AgentPropertiesInquiry,
+  AllPropertiesInquiry,
   PropertiesInquiry,
   PropertyInput,
 } from '../../libs/dto/property/property.input';
@@ -228,6 +229,43 @@ export class PropertyService {
     const sort: T = {
       [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC,
     };
+
+    const result = await this.propertyModel
+      .aggregate([
+        { $match: match },
+        { $sort: sort },
+        {
+          $facet: {
+            list: [
+              { $skip: (input.page - 1) * input.limit },
+              { $limit: input.limit },
+              lookupMember,
+              { $unwind: '$memberData' },
+            ],
+            metaCounter: [{ $count: 'total' }],
+          },
+        },
+      ])
+      .exec();
+
+    if (!result.length)
+      throw new InternalServerErrorException(Messages.NO_DATA_FOUND);
+
+    return result[0];
+  }
+
+  public async getAllPropertiesByAdmin(
+    input: AllPropertiesInquiry,
+  ): Promise<Properties> {
+    const { propertyStatus, propertyLocationList } = input.search;
+    const match: T = {};
+    const sort: T = {
+      [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC,
+    };
+
+    if (propertyStatus) match.propertyStatus = propertyStatus;
+    if (propertyLocationList)
+      match.propertyLocation = { $in: propertyLocationList };
 
     const result = await this.propertyModel
       .aggregate([
