02b1c7e92e048fdacbaa1a88aa605f489b96e185
Jasur(BEN) <abdujambulovj@gmail.com>
2026-04-18 15:59:48 +0900

    feat: develop updatePropertyByAdmin GraphQL api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -83,11 +83,22 @@ export class PropertyResolver {
   @Roles(MemberType.ADMIN)
   @UseGuards(RolesGuard)
   @Query((returns) => Properties)
-  public async getAllPropertiesByAdmin(
+  public async A(
     @Args('input') input: AllPropertiesInquiry,
     @AuthMember('_id') memberId: ObjectId,
   ): Promise<Properties> {
     console.log('Query: getAllPropertiesByAdmin');
     return await this.propertyService.getAllPropertiesByAdmin(input);
+  }
+
+  @Roles(MemberType.ADMIN)
+  @UseGuards(RolesGuard)
+  @Mutation((returns) => Property)
+  public async updatePropertyByAdmin(
+    @Args('input') input: PropertyUpdate,
+  ): Promise<Property> {
+    console.log('Mutation: updatePropertyByAdmin');
+    input._id = shapeIntoMongoObjectId(input._id);
+    return await this.propertyService.updatePropertyByAdmin(input);
   }
 }
