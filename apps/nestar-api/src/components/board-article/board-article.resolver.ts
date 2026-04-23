acac7596d6b5980fdaac4647379bd143610daadf
Jasur(BEN) <abdujambulovj@gmail.com>
2026-04-23 14:16:46 +0900

    feat: integrate like module created DTO

--- apps/nestar-api/src/components/board-article/board-article.resolver.ts
+++ apps/nestar-api/src/components/board-article/board-article.resolver.ts
@@ -1,21 +1,22 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { BoardArticleService } from './board-article.service';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/guards/auth.guard';
-import {
-  BoardArticle,
-  BoardArticles,
-} from '../../libs/dto/board-article/board-article';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { WithoutGuard } from '../auth/guards/without.guard';
+import { BoardArticleService } from './board-article.service';
+import { BoardArticleUpdate } from '../../libs/dto/board-article/board-article.update';
 import {
   AllBoardArticlesInquiry,
   BoardArticleInput,
   BoardArticlesInquiry,
 } from '../../libs/dto/board-article/board-article.input';
-import { AuthMember } from '../auth/decorators/authMember.decorator';
-import { ObjectId } from 'mongoose';
-import { WithoutGuard } from '../auth/guards/without.guard';
+import {
+  BoardArticle,
+  BoardArticles,
+} from '../../libs/dto/board-article/board-article';
 import { shapeIntoMongoObjectId } from '../../libs/config';
-import { BoardArticleUpdate } from '../../libs/dto/board-article/board-article.update';
+import { ObjectId } from 'mongoose';
+
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
