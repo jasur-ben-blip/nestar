843273aad66d0edf1e36940f69c14326795fadfa
Jasur(BEN) <abdujambulovj@gmail.com>
2026-04-24 21:36:30 +0900

    feat: integration of lookupAuthMemberLiked complex query business logic

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -21,7 +21,11 @@ import { ViewService } from '../view/view.service';
 import moment from 'moment';
 // import * as momnet from 'mongoose';
 import { PropertyUpdate } from '../../libs/dto/property/property.update';
-import { lookupMember, shapeIntoMongoObjectId } from '../../libs/config';
+import {
+  lookupAuthMemberLiked,
+  lookupMember,
+  shapeIntoMongoObjectId,
+} from '../../libs/config';
 import { LikeService } from '../like/like.service';
 import { LikeInput } from '../../libs/dto/like/like.input';
 import { LikeGroup } from '../../libs/enums/like.enum';
@@ -92,8 +96,6 @@ export class PropertyService {
       };
       targetProperty.meLiked =
         await this.likeService.checkLikeExistance(likeInput);
-
-      /** MEE-FOLLOWED **/
     }
 
     targetProperty.memberData = await this.memberService.getMember(
@@ -173,7 +175,8 @@ export class PropertyService {
             list: [
               { $skip: (input.page - 1) * input.limit },
               { $limit: input.limit },
-              // meLiked
+              /** ME-LIKED **/
+              lookupAuthMemberLiked(memberId),
               lookupMember,
               { $unwind: '$memberData' }, // [memberData] => memberdata
             ],
--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -36,6 +36,8 @@ export const availableCommentSorts = ['createdAt', 'updatedAt'];
 // IMAGE CONFIGURATION
 import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path';
+import { T } from './types/common';
+import { pipeline } from 'stream';
 
 export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
 export const getSerialForImage = (filename: string) => {
@@ -45,6 +47,43 @@ export const getSerialForImage = (filename: string) => {
 
 export const shapeIntoMongoObjectId = (target: any) => {
   return typeof target === 'string' ? new ObjectId(target) : target;
+};
+
+export const lookupAuthMemberLiked = (
+  memberId: T,
+  targetRefId: string = '$_id',
+) => {
+  return {
+    $lookup: {
+      from: 'likes',
+      let: {
+        locallikeRefId: targetRefId,
+        localMemberId: memberId,
+        localMyFavorite: true,
+      },
+      pipeline: [
+        {
+          $match: {
+            $expr: {
+              $and: [
+                { $eq: ['$likeRefId', '$$locallikeRefId'] },
+                { $eq: ['$memberId', '$$localMemberId'] },
+              ],
+            },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            memberId: 1,
+            likeRefId: 1,
+            myFavorite: '$$localMyFavorite',
+          },
+        },
+      ],
+      as: 'meLiked',
+    },
+  };
 };
 
 export const lookupMember = {
